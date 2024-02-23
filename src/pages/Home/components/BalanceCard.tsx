b8f39183fd453ae3f685f648120b76ceab4f1b35
Erik <erikhau3010@gmail.com>
2024-02-23 21:51:07 +0700

    feat: improve report page ui

--- src/pages/Home/components/BalanceCard.tsx
+++ src/pages/Home/components/BalanceCard.tsx
@@ -6,17 +6,29 @@ import { type TransactionType } from "@money-tracker-types/index";
 import { cn } from "@utils/index";
 
 interface BalanceCardProps {
+  dense?: boolean;
   label: string;
   type: TransactionType;
   balance: string;
 }
 
-const BalanceCard = ({ label, balance, type }: BalanceCardProps) => {
+const BalanceCard = ({
+  label,
+  balance,
+  type,
+  dense = false,
+}: BalanceCardProps) => {
   const isIncome = type === "income";
   const Icon = isIncome ? ArrowDownCircleIcon : ArrowUpCircleIcon;
 
   return (
-    <div className="overflow-hidden rounded-lg bg-white border hover:bg-gray-50 shadow-sm px-4 py-5 sm:p-4 flex items-center gap-3 cursor-pointer">
+    <div
+      className={cn(
+        "overflow-hidden rounded-lg bg-white border hover:bg-gray-50 shadow-sm px-4 py-5 flex items-center gap-3 cursor-pointer",
+        { "sm:p-4": !dense },
+        { "sm:py-3": dense }
+      )}
+    >
       <Icon
         className={cn("w-7 h-7", isIncome ? "fill-green-600" : "fill-red-600")}
       />
@@ -24,7 +36,12 @@ const BalanceCard = ({ label, balance, type }: BalanceCardProps) => {
         <div className="truncate text-xs font-medium text-gray-500">
           {label} Balance
         </div>
-        <div className="mt-1 text-xl font-semibold tracking-tight text-gray-900">
+        <div
+          className={cn("mt-1 font-semibold tracking-tight text-gray-900", {
+            "text-xl": !dense,
+            "text-base": dense,
+          })}
+        >
           <span className="text-xs text-gray-400 font-semibold">Rp</span>{" "}
           {balance}
         </div>
--- src/pages/Report/index.tsx
+++ src/pages/Report/index.tsx
@@ -1,21 +1,14 @@
 import SelectField from "@components/Shared/SelectField";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
+import BalanceCard from "@pages/Home/components/BalanceCard";
 import { noop } from "@utils/index";
 
 const Report = () => {
   return (
     <div className="space-y-6">
-      <div className="flex items-center justify-between">
-        <div className="flex flex-col space-y-1">
-          <div className="flex items-center gap-x-3">
-            <button className="p-0.5 rounded-full border text-gray-400 hover:bg-gray-100 flex items-center justify-center">
-              <ChevronLeftIcon className="w-5 h-5" />
-            </button>
-            <h1 className="font-semibold text-xl text-gray-900">March</h1>
-            <button className="p-0.5 rounded-full border text-gray-400 hover:bg-gray-100 flex items-center justify-center">
-              <ChevronRightIcon className="w-5 h-5" />
-            </button>
-          </div>
+      <section className="flex items-center justify-between">
+        <div className="space-y-1">
+          <h1 className="font-semibold text-xl text-gray-900">Report</h1>
           <p className="text-xs text-gray-400">Your weekly insight for March</p>
         </div>
         <SelectField
@@ -24,8 +17,41 @@ const Report = () => {
           dense
           items={["weekly", "monthly", "yearly"]}
         />
+      </section>
+      <div className="space-y-5">
+        <section className="flex justify-between items-center">
+          <div className="flex items-center gap-x-4">
+            <button className="p-0.5 rounded-full border text-gray-400 hover:bg-gray-100 flex items-center justify-center">
+              <ChevronLeftIcon className="w-5 h-5" />
+            </button>
+            <div className="space-y-1">
+              <p className="text-xs uppercase font-semibold text-gray-400">
+                March
+              </p>
+              <h1 className="font-semibold text-xl text-gray-900 space-x-1">
+                <span className="text-sm text-gray-400 font-semibold">Rp</span>
+                <span>250.000</span>
+              </h1>
+            </div>
+            <button className="p-0.5 rounded-full border text-gray-400 hover:bg-gray-100 flex items-center justify-center">
+              <ChevronRightIcon className="w-5 h-5" />
+            </button>
+          </div>
+          <div className="flex flex-col items-end space-y-1">
+            <p className="text-xs uppercase font-semibold text-gray-400">
+              Spend per day
+            </p>
+            <h1 className="font-semibold text-xl text-gray-900 space-x-1">
+              <span className="text-sm text-gray-400 font-semibold">Rp</span>
+              <span>250.000</span>
+            </h1>
+          </div>
+        </section>
+        <section className="grid grid-cols-1 xs:grid-cols-2 gap-4">
+          <BalanceCard dense type="income" label="Income" balance="10.000" />
+          <BalanceCard dense type="outcome" label="Outcome" balance="10.000" />
+        </section>
       </div>
-      <div>Hello world</div>
     </div>
   );
 };
