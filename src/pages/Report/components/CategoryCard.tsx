647f8d1c9eef88fd5a6a04206796de469208dca3
Erik <erikhau3010@gmail.com>
2024-03-11 18:13:23 +0700

    fix: ui

--- src/pages/Report/components/CategoryCard.tsx
+++ src/pages/Report/components/CategoryCard.tsx
@@ -1,10 +1,10 @@
 const CategoryCard = () => {
   return (
-    <div className="rounded-lg border flex flex-col p-5 cursor-pointer hover:bg-gray-50">
-      <div className="text-4xl w-16 h-16 flex items-center justify-center rounded-lg bg-blue-50/40 self-start">
+    <div className="rounded-lg border flex flex-col p-4 cursor-pointer hover:bg-gray-50">
+      <div className="text-3xl w-16 h-16 flex items-center justify-center rounded-lg bg-gray-100 border self-start">
         🍔
       </div>
-      <div className="mt-2.5 mb-3 space-y-1">
+      <div className="mt-2.5 mb-3 space-y-0.5">
         <h2 className="text-base font-semibold">Food & Beverage</h2>
         <h2 className="text-sm font-medium text-gray-500">
           <span className="text-xs text-gray-400 font-semibold">Rp</span>{" "}
