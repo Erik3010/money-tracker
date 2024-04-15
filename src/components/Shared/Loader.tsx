96979de5e2429bee8c23603c4d40a1af6826a76b
Erik <erikhau3010@gmail.com>
2024-04-15 18:02:08 +0700

    style: fix loader ui

--- src/components/Shared/Loader.tsx
+++ src/components/Shared/Loader.tsx
@@ -1,7 +1,7 @@
 const Loader = () => {
   return (
     <svg
-      className="animate-spin h-4 w-4 text-white stroke-[3]"
+      className="animate-spin h-6 w-6 text-white stroke-2"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
