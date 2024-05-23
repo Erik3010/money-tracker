b212112a1cbe965a178e7550b91b73fbbf729cde
Erik <erikhau3010@gmail.com>
2024-05-23 23:06:03 +0700

    feat: add forwardRef to Button.tsx

--- src/components/Shared/Button.tsx
+++ src/components/Shared/Button.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@utils/index";
 import Loader from "@components/Shared/Loader";
+import { forwardRef } from "react";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
@@ -9,24 +10,21 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 const buttonClass =
   "w-full h-11 rounded-md bg-blue-500 px-3.5 py-3.5 text-sm font-semibold text-white shadow-sm enabled:hover:bg-blue-600 leading-none flex justify-center items-center space-x-2.5 disabled:opacity-75 disabled:cursor-not-allowed";
 
-const Button = ({
-  className,
-  children,
-  loading = false,
-  ...props
-}: ButtonProps) => {
-  return (
-    <button className={cn(buttonClass, className)} {...props}>
-      {loading ? (
-        <Loader
-          className="h-6 w-6 text-white stroke-2"
-          strokeColor="stroke-white"
-        />
-      ) : (
-        <span>{children}</span>
-      )}
-    </button>
-  );
-};
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, loading = false, ...props }: ButtonProps, ref) => {
+    return (
+      <button ref={ref} className={cn(buttonClass, className)} {...props}>
+        {loading ? (
+          <Loader
+            className="h-6 w-6 text-white stroke-2"
+            strokeColor="stroke-white"
+          />
+        ) : (
+          <span>{children}</span>
+        )}
+      </button>
+    );
+  }
+);
 
 export default Button;
